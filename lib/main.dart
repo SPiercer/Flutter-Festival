e4ac55fd7c5d8d8245d73286c3b741eeb1ae573d
SPiercer <snowpiercer2001@gmail.com>
2022-03-24 22:32:43 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Flutter Festival ❤️'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String result = 'Select an Operation';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = Operations.add(2, 2).toString();
+                  });
+                },
+                child: const Text("2 + 2")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = Operations.multiply(2, 2).toString();
+                  });
+                },
+                child: const Text("2 × 2")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = Operations.divide(2, 2).toString();
+                  });
+                },
+                child: const Text("2 ÷ 2")),
+            Text(
+              result,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+abstract class Operations {
+  static num add(num left, num right) => (left + right);
+  static num multiply(num left, num right) => (left * right);
+  static num divide(num left, num right) => (left / right);
+}
